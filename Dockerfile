8bc9aba6d1eada377fea410190d7497b981918d5
nasir <nasir.ciem.it@gmail.com>
2023-09-08 22:51:11 +0530

    docker running

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3-alpine
 WORKDIR /src
 COPY . .
-RUN pipenv install
+RUN pip install -r requirements.txt
 EXPOSE 5000
 CMD [ "flask","run","--host","0.0.0.0","--port","5000" ]
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+
 class Config:
     JWT_SECRET_KEY = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
-    MONGO_URI = "mongodb://db:27017/flask-auth"
+    MONGO_URI = "mongodb://admin:secret@db:27017/flask-auth?authSource=admin"
